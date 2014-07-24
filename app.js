df12200a11cfbba21a5831b2740b4c6752bd9d51
Darren Royle <darren.royle@oup.com>
2014-07-24 13:44:13 +0100

    Getting set up for Heroku deployment

--- app.js
+++ app.js
@@ -15,8 +15,10 @@ app.set('view engine', 'jade');
 
 // app.use(favicon(__dirname + '/public/img/favicon.ico'));
 app.use(logger('dev'));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
